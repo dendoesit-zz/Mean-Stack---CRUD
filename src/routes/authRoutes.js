7cca07980418ea492fa851b9a187ecbc7a8805d0
dendoesit <dan_claw20@yahoo.com>
2016-03-01 18:47:11 +0100

    nothing

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,4 +1,3 @@
-
 var express = require('express');
 var authRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
@@ -12,9 +11,9 @@ var router = function () {
             mongodb.connect(url, function (err, db) {
                 var collection = db.collection('users');
                 var user = {
-                        username: req.body.userName,
-                        password: req.body.password
-                    };
+                    username: req.body.userName,
+                    password: req.body.password
+                };
                 collection.insert(user,
                     function (err, results) {
                         req.login(results.ops[0], function () {
@@ -22,7 +21,7 @@ var router = function () {
                         });
                     });
             });
-            
+
         });
     authRouter.route('/signIn')
         .post(passport.authenticate('local', {
@@ -31,13 +30,13 @@ var router = function () {
             res.redirect('/auth/profile');
         });
     authRouter.route('/profile')
-    .all(function(req,res,next) {
+        .all(function (req, res, next) {
             if (!req.user) {
                 res.redirect('/');
             }
-        next();
+            next();
         })
-    .get(function (req, res) {
+        .get(function (req, res) {
             res.json(req.user);
         });
     return authRouter;
